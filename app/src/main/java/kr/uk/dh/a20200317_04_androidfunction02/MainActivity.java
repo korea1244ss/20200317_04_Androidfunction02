b83b49fc1212c3df708c7c1b4ccfb9d560624e84
korea1244ss <61854931+korea1244ss@users.noreply.github.com>
2020-03-17 17:04:30 +0900

    Initial commit

---
+++ app/src/main/java/kr/uk/dh/a20200317_04_androidfunction02/MainActivity.java
@@ -0,0 +1,24 @@
+package kr.uk.dh.a20200317_04_androidfunction02;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import kr.uk.dh.a20200317_04_androidfunction02.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+
+
+
+    }
+}
